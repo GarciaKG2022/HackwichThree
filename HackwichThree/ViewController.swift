f8450a135a8ff6212fa80b0cba7961ac51787cc2
GarciaKG2022 <garciakg@hawaii.edu>
2022-02-09 21:57:52 -0600

    Completed On My Own Portion

--- HackwichThree/ViewController.swift
+++ HackwichThree/ViewController.swift
@@ -26,7 +26,50 @@ class ViewController: UIViewController {
             
             self.view.backgroundColor = UIColor.blue
         }
-    
+// On My Own
     }
-}
+// Problem Set 1
+    var bubbles = "She has the laughter"
+    var buttercup = "She is the strongest"
+    
+    @IBOutlet var labelOne: UILabel!
+    
+    if bubbles == "She has the laughter"
+    {
+        self.view.backgroundColor = UIColor.red
+        
+    } else {
+        
+        self.view.backgroundColor = UIColor.green
+    }
 
+    @IBOutlet var labelTwo: UILabel!
+    
+    if buttercup == "She is the strongest"
+    {
+        self.view.backgroundColor = UIColor.orange
+        
+    } else {
+        
+        self.view.backgroundColor = UIColor.blue
+    }
+        
+        
+//Problem Set 2
+    
+    var hot = "How is the weather"
+    var cold = "How is the weather"
+    
+    @IBAction func magicButtonPressed(_ sender: Any) {
+        
+//Problem Set 3
+        
+        if hot == cold
+    {
+        self.view.backgroundColor = UIColor.green
+            
+        print("I completed both problem sets")
+            
+    }
+  }
+}
